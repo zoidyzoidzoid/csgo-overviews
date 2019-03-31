f60550b623a37db2f2e63634e3a6f4c95de0ce41
William Stewart <zoidbergwill@gmail.com>
2019-03-31 15:02:24 +0200

    Work it

--- scripts/update_overviews.sh
+++ scripts/update_overviews.sh
@@ -4,8 +4,16 @@ BASE_DIR="$(realpath $(dirname $0)/..)"
 
 cd "${BASE_DIR}"
 
-for file in $(ls "${BASE_DIR}/overviews/raw/*_radar.dds"); do
+for file in $(ls ${BASE_DIR}/overviews/raw/*_radar.dds); do
     target="${BASE_DIR}/overviews/$(basename "${file}" _radar.dds).jpg"
+    name="$(basename "${target}" .jpg)"
     echo "Creating $(basename "${target}")"
     convert "${file}" "${target}"
+    case "${name}" in
+        de_lake|de_mirage)
+	    convert -flip "${target}" "${target}"
+	    ;;
+    esac
+    cjpeg -outfile "${target}.bu" "${target}"
+    mv "${target}.bu" "${target}"
 done
